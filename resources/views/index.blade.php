7c5a61b93863d392ad4e96e6938243f57ef9e26c
Victor Monsalves Rivas <vitoxint@gmail.com>
2021-11-16 09:46:31 -0300

    first-commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/app.css" >
+    <title>Segic chat</title>
+</head>
+<body>
+    <div class="app">
+        <header>
+            <h1>Segic CHAT </h1>
+            <input type="text" name="username" id="username" placeholder="super nickname" /> 
+        </header>
+
+        <div id="messages"></div>
+        <form id="message_form" >
+            <input type="text" name="message" id="message-input" placeholder="enter a message">
+            <button type="submit" id="message_send">Send</button>
+        </form>
+    </div>
+
+
+
+    <script src="./js/app.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Events\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::post('/send-message' , function( Request $request ){
+
+    event(
+        new Message(
+            $request->input('username'),
+            $request->input('message')
+        )
+
+    );
+
+    return [ 'success' => true ];
+});
