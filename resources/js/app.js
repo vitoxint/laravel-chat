41e6f335291959a615c3af46edc2658895483fc3
Victor Monsalves Rivas <vitoxint@gmail.com>
2021-11-16 18:40:03 -0300

    peticion hhtp a chat 4

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,32 +3,32 @@ require('./bootstrap');
 //const { default : axios } = require('axios');
 
 //DOM JS
-const message_el = document.getElementById('messages');
-const username_input = document.getElementById('username');
-const message_input = document.getElementById('message_input');
-const message_form = document.getElementById('message_form');
+const message_el = document.getElementById('messages')
+const username_input = document.getElementById('username')
+const message_input = document.getElementById('message_input')
+const message_form = document.getElementById('message_form')
 
 message_form .addEventListener('submit' , function(e){
     e.preventDefault();
 
-    let hasError = false;
+    let hasError = false
 
     if( username_input.value == ''){
-        alert('Enter a username');
+        alert('Enter a username')
         hasError = true
     }
 
     if( message_input.value == ''){
-        alert('Enter a message');
+        alert('Enter a message')
         hasError = true
     }
     if(hasError){
 
-        return ;
+        return 
 
     }
 
-    const option = {
+    const options = {
         method : 'post',
         url: '/send-message',
         data: {
@@ -38,7 +38,7 @@ message_form .addEventListener('submit' , function(e){
             
     }
 
-    window.axios( option );
+    window.axios( options );
 
     console.log(username_input.value);
     console.log(message_input.value);
